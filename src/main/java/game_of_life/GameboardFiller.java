013ec1323b0e5905cf286a42430a8e24c5f90384
Weronika Mirek <werussia16@gmail.com>
2019-08-13 15:50:19 +0200

    GameboardFiller checking if pattern is within the gameboard bounds

--- src/main/java/game_of_life/GameboardFiller.java
+++ src/main/java/game_of_life/GameboardFiller.java
@@ -32,20 +32,34 @@ class GameboardFiller {
         }
     }
 
+    private boolean checkIfPositionIsOnGameboard(PatternOnPosition patternOnPosition) {
+        boolean[][] pattern = patternOnPosition.getPattern();
+        int positionX = patternOnPosition.getPosition().getPositionX();
+        int positionY = patternOnPosition.getPosition().getPositionY();
+        int padding = 2 * GameOfLife.BOARD_PADDING;
+        if (positionX > 0 && positionX < gameboard[0].length - padding && positionY > 0 && positionY < gameboard.length - padding) {
+            if (positionX + pattern[0].length < gameboard[0].length - padding && positionY + pattern.length < gameboard.length - padding) {
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+
     private void fillWithSetOfPatterns(Set<PatternOnPosition> patternsOnPositions) {
         for (PatternOnPosition patternOnPosition : patternsOnPositions) {
             boolean[][] pattern = patternOnPosition.getPattern();
             int positionX = patternOnPosition.getPosition().getPositionXWithPadding();
             int positionY = patternOnPosition.getPosition().getPositionYWithPadding();
-            try {
+            if (checkIfPositionIsOnGameboard(patternOnPosition)) {
                 for (int i = positionY; i < positionY + pattern.length; i++) {
                     System.arraycopy(pattern[i - positionY], 0, gameboard[i], positionX, positionX + pattern[0].length - positionX);
                 }
-            }catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("The position is out of gameboard bounds!");
-                return;
+            } else {
+                System.out.println("One of the patterns couldn't be drawn, because it was out of gameboard!");
             }
-
         }
     }
 }
