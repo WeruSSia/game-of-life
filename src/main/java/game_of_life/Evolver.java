89cc9efc4d9c3e52ee517b4d51878f87a625608a
Weronika Mirek <werussia16@gmail.com>
2019-08-27 14:34:39 +0200

    Evolver implementation, GameOfLife adjusted to Evolver

--- src/main/java/game_of_life/Evolver.java
+++ src/main/java/game_of_life/Evolver.java
@@ -4,23 +4,35 @@ import java.util.TimerTask;
 
 public class Evolver extends TimerTask {
 
-    private GameOfLife gameOfLife;
-    private boolean[][] gameboard = gameOfLife.getGameboardCopy();
+    private GameVariationSettings gameVariationSettings;
+    private boolean[][] gameboard;
 
+    Evolver(GameVariationSettings gameVariationSettings, boolean[][] gameboard) {
+        this.gameVariationSettings = gameVariationSettings;
+        this.gameboard = gameboard;
+    }
 
     @Override
     public void run() {
+        System.out.println(toString());
         evolve();
-
     }
 
     private void evolve() {
-        boolean[][] gameboardCopy = gameOfLife.getGameboardCopy();
+        boolean[][] gameboardCopy = getGameboardCopy();
         for (int i = 1; i < gameboard.length - 1; i++) {
             for (int j = 1; j < gameboard[i].length - 1; j++) {
                 gameboard[i][j] = willSurviveToNextGeneration(gameboardCopy, i, j);
             }
         }
+    }
+
+    private boolean[][] getGameboardCopy() {
+        boolean[][] gameboardCopy = new boolean[gameboard.length][gameboard[0].length];
+        for (int i = 0; i < gameboard.length; i++) {
+            System.arraycopy(gameboard[i], 0, gameboardCopy[i], 0, gameboard[i].length);
+        }
+        return gameboardCopy;
     }
 
     private boolean willSurviveToNextGeneration(boolean[][] gameboardCopy, int i, int j) {
@@ -56,5 +68,19 @@ public class Evolver extends TimerTask {
             counter++;
         }
         return counter;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = GameOfLife.BOARD_PADDING; i < gameboard.length - GameOfLife.BOARD_PADDING; i++) {
+            boolean[] row = gameboard[i];
+            for (int j = GameOfLife.BOARD_PADDING; j < row.length - GameOfLife.BOARD_PADDING; j++) {
+                boolean cell = row[j];
+                stringBuilder.append(cell ? "0" : ".").append("  ");
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
     }
 }
--- src/main/java/game_of_life/GameOfLife.java
+++ src/main/java/game_of_life/GameOfLife.java
@@ -1,5 +1,7 @@
 package game_of_life;
 
+import java.util.Timer;
+
 public class GameOfLife {
 
     private boolean[][] gameboard;
@@ -18,35 +20,11 @@ public class GameOfLife {
     public void play() {
         generateGameboard(gameboardResolutionSettings);
         new GameboardFiller(gameboard, gameboardFillingSettings).fill();
-        for (int i = 0; i < 20; i++) {
-            System.out.println(toString());
-            evolve();
-        }
+        Timer timer = new Timer();
+        timer.schedule(new Evolver(gameVariationSettings, gameboard), 0, 1000);
     }
 
     private void generateGameboard(GameboardResolutionSettings gameboardResolutionSettings) {
         gameboard = new boolean[gameboardResolutionSettings.getLogicWidth() + (2 * BOARD_PADDING)][gameboardResolutionSettings.getLogicHeight() + (2 * BOARD_PADDING)];
-    }
-
-    boolean[][] getGameboardCopy() {
-        boolean[][] gameboardCopy = new boolean[gameboard.length][gameboard[0].length];
-        for (int i = 0; i < gameboard.length; i++) {
-            System.arraycopy(gameboard[i], 0, gameboardCopy[i], 0, gameboard[i].length);
-        }
-        return gameboardCopy;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = BOARD_PADDING; i < gameboard.length - BOARD_PADDING; i++) {
-            boolean[] row = gameboard[i];
-            for (int j = BOARD_PADDING; j < row.length - BOARD_PADDING; j++) {
-                boolean cell = row[j];
-                stringBuilder.append(cell ? "0" : ".").append("  ");
-            }
-            stringBuilder.append("\n");
-        }
-        return stringBuilder.toString();
     }
 }
